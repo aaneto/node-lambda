99df9524825f2590ba20cd80bb73e9c06c1cb9aa
Adison Neto <almeidneto@gmail.com>
2020-12-02 08:50:38 -0300

    optionally pass deploy role by env var

--- src/deploy.ts
+++ src/deploy.ts
@@ -7,15 +7,21 @@ program.version(version);
 program
     .requiredOption("-f, --function-name <string>", "name of the function to deploy")
     .requiredOption("-b, --build-folder <string>", "folder where the built files are")
-    .requiredOption("-r, --role <string>", "role of the function to deploy");
+    .option("-r, --role <string>", "role of the function to deploy");
 
 export default async function() {
     program.parse(process.argv);
+
+    const role = program.role || process.env.AWS_ROLE;
+    if (!role) {
+        throw "The lambda role must be provided! Either by CLI argument or by environment variable."
+    }
+
     const res = await deployFunction({
-        name: program.functionName,
-        role: program.role,
         codePath: program.buildFolder,
+        name: program.functionName,
         description,
+        role
     });
     console.log(res);
 }
